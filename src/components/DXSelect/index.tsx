4807f0752a159ce722dc9aa2d81b876940d3182d
duxinyues <yongyuan26445@gmail.com>
2023-07-14 01:04:34 +0800

    feat:Select组件二次封装

---
+++ src/components/DXSelect/index.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Select } from "antd";
+
+interface optionItem {
+    itemKey?: string,
+    itemValue?: string
+}
+interface ConfigProps {
+    options: optionItem[],// 下拉框选项
+    styles?: object, // 宽度
+    otherProps?: object, // 其他属性
+}
+interface Props {
+    config: ConfigProps
+}
+const DXSelect: React.FC<Props> = ({ config }) => {
+    const { styles = { width: "200px" }, options, otherProps } = config
+    return <Select style={styles} {...otherProps}>
+        {
+            options.map((item: any) => <Select.Option key={item.itemKey} value={item.itemKey}>{item.itemValue}</Select.Option>)
+        }
+    </Select>
+}
+export default DXSelect;
