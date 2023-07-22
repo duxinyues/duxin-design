3ca094590b2fe4a153d1338e42305f68ea276b48
duxinyues <yongyuan26445@gmail.com>
2023-07-22 23:32:07 +0800

    fix:优化字典组件

--- src/components/DXSelect/index.tsx
+++ src/components/DXSelect/index.tsx
@@ -9,12 +9,18 @@ interface ConfigProps {
     options: optionItem[],// 下拉框选项
     styles?: object, // 宽度
     otherProps?: object, // 其他属性
+    type?:string, // 类型：text
 }
 interface Props {
     config: ConfigProps
 }
 const DXSelect: React.FC<Props> = ({ config }) => {
-    const { styles = { width: "200px" }, options, otherProps } = config
+    const { styles = { width: "200px" }, options, otherProps,type } = config;
+
+    if(type){
+        
+    }
+
     return <Select style={styles} {...otherProps}>
         {
             options.map((item: any) => <Select.Option key={item.itemKey} value={item.itemKey}>{item.itemValue}</Select.Option>)
